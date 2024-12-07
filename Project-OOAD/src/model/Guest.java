ee67e1fd99f5d5a84ea4484e6dba4e87c2eaf5fe
PHITSUH <joelnathan98@gmail.com>
2024-12-07 14:22:19 +0700

    edit dikit

--- Project-OOAD/src/model/Guest.java
+++ Project-OOAD/src/model/Guest.java
@@ -12,14 +12,19 @@ public class Guest extends User {
 	}
 
 	public static List<User> getGuestsByTransactionID(String eventID) {
-		String query = "SELECT UserId FROM Invitation WHERE EventId LIKE '" + eventID
-				+ "' AND InvitationRole LIKE 'Guest' AND InvitationStatus LIKE 'Accepted'";
+		String query = "SELECT User.* FROM Invitation Join User ON Invitation.UserId = User.UserId WHERE EventId LIKE '"
+				+ eventID + "' AND InvitationRole LIKE 'Guest' AND InvitationStatus LIKE 'Accepted'";
 		connect.rs = connect.execQuery(query);
 		List<User> guestList = new ArrayList<>();
 		try {
 			while (connect.rs.next()) {
 				String userId = connect.rs.getString("userId");
-				guestList.add(getGuestById(userId));
+				String email = connect.rs.getString("UserEmail");
+				String name = connect.rs.getString("UserName");
+				String pass = connect.rs.getString("UserPassword");
+				String role = connect.rs.getString("UserPassword");
+
+				guestList.add(new Guest(userId, email, name, pass, role));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -27,23 +32,6 @@ public class Guest extends User {
 		}
 
 		return guestList;
-	}
-
-	public static Guest getGuestById(String id) {
-		String query = "SELECT * FROM User WHERE UserId LIKE '" + id + "'";
-		connect.rs = connect.execQuery(query);
-		try {
-			String email = connect.rs.getString("UserEmail");
-			String name = connect.rs.getString("UserName");
-			String pass = connect.rs.getString("UserPassword");
-			String role = connect.rs.getString("UserPassword");
-			return new Guest(id, email, name, pass, role);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-
 	}
 
 }
