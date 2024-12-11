d54f301fd1fc33fb370a9e6986afb3da1659c738
PHITSUH <joelnathan98@gmail.com>
2024-12-11 16:37:47 +0700

    tes

--- Project-OOAD/src/controller/InvitationController.java
+++ Project-OOAD/src/controller/InvitationController.java
@@ -7,17 +7,14 @@ import model.Invitation;
 public class InvitationController {
 
 	public InvitationController() {
-		
 
 	}
-	
 
 	public static ArrayList<Invitation> getInvitation(String email) {
 		// TODO Auto-generated method stub
 		return Invitation.getInvitations(email);
 		return Invitation.getInvitationsByEmail(email);
 	}
-	
 
 	public static String acceptInvitation(String InvitationId) {
 		return Invitation.acceptInvitation(InvitationId);
