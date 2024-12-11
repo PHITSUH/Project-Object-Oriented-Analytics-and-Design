4e0a7c0f7bce57a52d601e90a960de4b3c3e1c4f
PHITSUH <joelnathan98@gmail.com>
2024-12-11 16:33:01 +0700

    Update Invitation

--- Project-OOAD/src/controller/InvitationController.java
+++ Project-OOAD/src/controller/InvitationController.java
@@ -13,8 +13,10 @@ public class InvitationController {
 	public static ArrayList<Invitation> getInvitation(String email) {
 		// TODO Auto-generated method stub
 		return Invitation.getInvitations(email);
+		return Invitation.getInvitationsByEmail(email);
 	}
 	
+
 	public static String acceptInvitation(String InvitationId) {
 		return Invitation.acceptInvitation(InvitationId);
 	}
